f27ce8414191223bb192a47baeeaaf3cbce152ac
RegisSalesRA <regismsales@gmail.com>
2021-04-21 06:49:55 -0300

    insert v1 project files

--- server/urls.py
+++ server/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),
     path('api-token-auth/', views.obtain_auth_token)
 ]
