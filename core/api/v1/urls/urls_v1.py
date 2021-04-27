78546bd65452396707fcecdc71ad9d4a7a5f58fc
RegisSalesRA <regismsales@gmail.com>
2021-04-27 01:24:05 -0300

    url and view favorite

--- core/api/v1/urls/urls_v1.py
+++ core/api/v1/urls/urls_v1.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from core.api.v1.views.views import BookView
+from core.api.v1.views.views import BookView,FavoriteView
 
 urlpatterns = [
     path('books/',BookView.as_view()),
+    path('favorite/',FavoriteView.as_view()),
 ]
--- core/api/v1/views/views.py
+++ core/api/v1/views/views.py
@@ -25,4 +25,28 @@ class BookView(APIView):
         return Response(serializers.data)
 
 
-       
+class FavoriteView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication,]
+
+    def post(self, request):
+        data = request.data["id"]
+        # print(data)
+        try:
+            book_obj = Book.objects.get(id=data)
+            # print(data)
+            user = request.user
+            single_favorite_book = Favorite.objects.filter(
+                user=user).filter(book=book_obj).first()
+            if single_favorite_book:
+                print("single_favorite_book")
+                ccc = single_favorite_book.isFavorit
+                single_favorite_book.isFavorit = not ccc
+                single_favorite_book.save()
+            else:
+                Favorite.objects.create(
+                    book=book_obj, user=user, isFavorit=True)
+            response_msg = {'error': False}
+        except:
+            response_msg = {'error': True}
+        return Response(response_msg)
