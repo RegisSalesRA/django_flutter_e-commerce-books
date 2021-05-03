520f328c8957d539cd915897188cf54193363205
RegisSalesRA <regismsales@gmail.com>
2021-05-02 21:06:23 -0300

    user , book, drower,single_book

---
+++ client/lib/widgets/drower.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:client/screens/favorite_screen.dart';
+import 'package:client/screens/home_screen.dart';
+import 'package:client/screens/login_screen.dart';
+import 'package:localstorage/localstorage.dart';
+
+class AppDrown extends StatefulWidget {
+  @override
+  _AppDrownState createState() => _AppDrownState();
+}
+
+class _AppDrownState extends State<AppDrown> {
+  LocalStorage storage = new LocalStorage('usertoken');
+
+  void _logoutnew() async {
+    await storage.clear();
+    Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: Text("Welcome"),
+            automaticallyImplyLeading: false,
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(HomeScreen.routeName);
+            },
+            trailing: Icon(
+              Icons.home,
+              size: 30,
+              color: Colors.blue,
+            ),
+            title: Text(
+              "Home",
+              style: TextStyle(fontSize: 18, color: Colors.blue),
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(FavoriteScreen.routeName);
+            },
+            trailing: Icon(
+              Icons.star,
+              color: Colors.yellow,
+              size: 30,
+            ),
+            title: Text(
+              "Favorite",
+              style: TextStyle(fontSize: 18, color: Colors.blue),
+            ),
+          ),
+          Spacer(),
+          ListTile(
+            onTap: () {
+              _logoutnew();
+            },
+            trailing: Icon(
+              Icons.logout,
+              color: Colors.red,
+            ),
+            title: Text(
+              "Logout",
+              style: TextStyle(fontSize: 18, color: Colors.red),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+//OrderScreens
---
+++ client/lib/widgets/single_book.dart
@@ -0,0 +1,45 @@
+import 'package:client/api/book_api.dart';
+import 'package:client/screens/book_detail.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SingleBook extends StatelessWidget {
+  final int id;
+  final String title;
+  final String image;
+  final bool favorite;
+
+  const SingleBook({Key key, this.id, this.title, this.image, this.favorite})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed(
+            BookDetails.routeName,
+            arguments: id,
+          );
+        },
+        child: Image.network(
+          "http://10.0.2.2:8000$image",
+          fit: BoxFit.cover,
+        ),
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        title: Text(title),
+        leading: IconButton(
+          onPressed: () {
+            Provider.of<BookState>(context, listen: false).favoriteButton(id);
+          },
+          icon: Icon(
+            favorite ? Icons.star : Icons.star_border,
+            color: Colors.yellow,
+          ),
+        ),
+      ),
+    );
+  }
+}
