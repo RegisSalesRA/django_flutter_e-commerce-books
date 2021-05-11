61e483b81249d47826cb1eb251c1b1f749d81df8
RegisSalesRA <regismsales@gmail.com>
2021-05-10 21:40:06 -0300

    Swagger

--- core/api/v1/urls/urls_v1.py
+++ core/api/v1/urls/urls_v1.py
@@ -1,6 +1,24 @@
 from django.urls import path
 from core.api.v1.views.views import *
 from rest_framework.authtoken.views import obtain_auth_token
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('books/',BookView.as_view()),
@@ -14,4 +32,8 @@ urlpatterns = [
     path('delatecartbooks/', DelateCarBook.as_view()),
     path('deletecart/', DelateCart.as_view()),
     path('ordernow/', OrderCreate.as_view()),
+    path('swagger_v1', schema_view.with_ui('swagger',
+                                 cache_timeout=0), name='schema-swagger-ui'),
+    path("redoc", schema_view.with_ui('redoc',
+                                      cache_timeout=0), name='schema-redoc'),
 ]
--- server/settings.py
+++ server/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'core',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_yasg',
 
 ]
 
