8f31308c2331d723c7c5e7ab39a59a54bd2694b2
Regis Sales <regis@prodadospesquisa.com.br>
2021-05-12 17:11:39 -0300

    login layout finished

--- client/lib/screens/login_screen.dart
+++ client/lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:client/api/user_api.dart';
 import 'package:client/screens/home_screen.dart';
 import 'package:client/screens/register_screen.dart';
@@ -49,107 +51,152 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Login Now"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(12),
-        child: SingleChildScrollView(
-          child: Form(
-            key: _form,
-            child: Column(
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/livrologin.webp"),
+                    fit: BoxFit.cover)),
+            child: ListView(
               children: [
-                Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Icon(
-                    Icons.account_circle,
-                    color: Colors.blueAccent,
-                    size: 150,
-                  ),
-                ),
-                Padding(
-                    child: TextFormField(
-                      validator: (v) {
-                        if (v.isEmpty) {
-                          return "Enter your username";
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                          contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                          hintText: "Username",
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(32))),
-                      onSaved: (v) {
-                        _username = v;
-                      },
-                    ),
-                    padding: EdgeInsets.all(15)),
-                Padding(
-                  padding: EdgeInsets.all(16),
-                  child: TextFormField(
-                    validator: (v) {
-                      if (v.isEmpty) {
-                        return "Enter your password";
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                        hintText: "Password",
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(32))),
-                    onSaved: (v) {
-                      _password = v;
-                    },
-                    obscureText: true,
-                  ),
-                ),
-                // botoes
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                Stack(
                   children: [
-                    // ignore: deprecated_member_use
-                    RaisedButton(
-                      onPressed: () {
-                        _loginNew();
-                      },
-                      child: Text(
-                        "login",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                      color: Colors.blue,
-                      padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(32)),
-                    ),
-                    // ignore: deprecated_member_use
-                    FlatButton(
-                      onPressed: () {
-                        Navigator.of(context)
-                            .pushReplacementNamed(RegisterScreen.routeName);
-                      },
-                      child: Text(
-                        "Register",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                      color: Colors.blue,
-                      padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(32)),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Form(
+                            key: _form,
+                            child: Column(
+                              children: [
+                                //Form
+                                Container(
+                                    padding: EdgeInsets.all(15),
+                                    decoration: BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                color: Colors.white24,
+                                                width: 0.1))),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 10),
+                                          child: TextFormField(
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                            validator: (v) {
+                                              if (v.isEmpty) {
+                                                return "Enter your username";
+                                              }
+                                              return null;
+                                            },
+                                            decoration: InputDecoration(
+                                                icon: Icon(
+                                                  Icons.account_circle,
+                                                  color: Colors.white,
+                                                ),
+                                                hintText: "Username",
+                                                filled: true,
+                                                hintStyle: (TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 15)),
+                                                border: InputBorder.none),
+                                            onSaved: (v) {
+                                              _username = v;
+                                            },
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 16),
+                                          child: TextFormField(
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                            validator: (v) {
+                                              if (v.isEmpty) {
+                                                return "Enter your password";
+                                              }
+                                              return null;
+                                            },
+                                            decoration: InputDecoration(
+                                                icon: Icon(
+                                                  Icons.lock_outline,
+                                                  color: Colors.white,
+                                                ),
+                                                hintText: "Password",
+                                                hintStyle: (TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 15)),
+                                                filled: true,
+                                                border: InputBorder.none),
+                                            onSaved: (v) {
+                                              _password = v;
+                                            },
+                                            obscureText: true,
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 20),
+                                          child: InkWell(
+                                            onTap: () {
+                                              _loginNew();
+                                            },
+                                            child: Container(
+                                              width: 200,
+                                              height: 60,
+                                              alignment: Alignment.center,
+                                              decoration: BoxDecoration(
+                                                  gradient: LinearGradient(
+                                                      colors: [
+                                                        Colors.red,
+                                                        Colors.orange
+                                                      ]),
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(
+                                                              30.0))),
+                                              child: Text(
+                                                "Sign in",
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.w300,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+
+                                        //BOTAO CADASTRE
+                                        // ignore: deprecated_member_use
+                                        FlatButton(
+                                          onPressed: () {
+                                            Navigator.of(context)
+                                                .pushReplacementNamed(
+                                                    RegisterScreen.routeName);
+                                          },
+                                          child: Text(
+                                            "clique aqui para se cadastrar",
+                                            textAlign: TextAlign.center,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w300,
+                                                color: Colors.white,
+                                                fontSize: 10),
+                                          ),
+                                        )
+                                      ],
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     )
                   ],
-                )
+                ),
               ],
-            ),
-          ),
-        ),
-      ),
-    );
+            )));
   }
 }
